bed843d9851c35e7553779b76e513a1a860cade4
matias cox <mcox5@uc.cl>
2024-10-01 18:57:48 -0300

    feat(whatsapp-webhook): implement whatsapp webhook controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   get "googleauth" => "google_auth#callback"
+  match '/webhook', to: 'whatsapp#receive', via: [:get, :post]
   # Defines the root path route ("/")
   # root "posts#index"
 end
