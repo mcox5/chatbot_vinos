b4052543afaff728591fb1ecdc3af8b09f169fd2
matias cox <mcox5@uc.cl>
2024-10-01 18:59:50 -0300

    feat(whatsapp-errors): create base whatsapp errors

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -2,3 +2,8 @@ module GoogleError
   class BaseGoogleError < StandardError; end
   class AuthenticationGoogleError < BaseGoogleError; end
 end
+
+module WhatsAppError
+  class BaseWhatsappError < StandardError; end
+  class SendMessageWhatsappError < BaseWhatsappError; end
+end
